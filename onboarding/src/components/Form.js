7cb53f1a534f236bf3f531bf83e13f6d56b0db30
Joscelyn <JoscelynRose@gmail.com>
2019-08-07 15:57:33 -0400

    added password verification

--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -28,6 +28,10 @@ const Form = ({ errors, touched, values, handleSubmit, status }) => {
         {touched.password && errors.password && (
           <p className="error">{errors.password}</p>
         )}
+        <Field type="password" name="passwordverify" placeholder="Password" />
+        {touched.passwordverify && errors.passwordverify && (
+          <p className="error">{errors.passwordverify}</p>
+        )}
 
         <label className="checkbox-container">
           I understand the terms of service
@@ -54,17 +58,26 @@ const Form = ({ errors, touched, values, handleSubmit, status }) => {
 // Function that takes in a component, extends some logic onto that component,
 // returns a _new_ component (copy of the passed in component with the extended logic)
 const FormikUserForm = withFormik({
-  mapPropsToValues({ name, email, termsOfService }) {
+  mapPropsToValues({ name, email, password, passwordverify, termsOfService }) {
     return {
       name: name || "",
       email: email || "",
+      password: password || "",
+      passwordverify: passwordverify || "",
       termsOfService: termsOfService || false
     };
   },
 
   validationSchema: Yup.object().shape({
     name: Yup.string().required(),
-    email: Yup.string().required()
+    email: Yup.string().required(),
+    password: Yup.string()
+      .required()
+      .min(8, "Password must be 8 characters or longer"),
+    passwordverify: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "Passwords must match"
+    )
   }),
 
   handleSubmit(values, { setStatus }) {
